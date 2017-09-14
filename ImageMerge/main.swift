d119f2dfdbff263c14da320460d79541e3fde50d
Geraldo Fernandes <geraldofernandes@gmail.com>
2017-09-14 14:31:06 -0300

    Initial Commit

---
+++ ImageMerge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ImageMerge
+//
+//  Created by Geraldo Fernandes on 14/09/17.
+//  Copyright © 2017 Geraldo Fernandes. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
