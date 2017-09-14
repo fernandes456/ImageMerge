df55fa7d4d22a9d5c142cd977c0c04ec531812ae
Geraldo Fernandes <geraldofernandes@gmail.com>
2017-09-14 18:59:05 -0300

    second commit

--- ImageMerge/Images.swift
+++ ImageMerge/Images.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import AppKit
+import CoreImage
+
+struct Images {
+    
+    func generate() {
+        let bundle = Bundle.main
+        let firstImagePath = bundle.path(forResource: "nfl1", ofType: "png")!
+        let secondImagePath = bundle.path(forResource: "nfl2", ofType: "png")!
+        
+        let firstImageURL = URL(string: firstImagePath)!
+        let secondImageURL = URL(string: secondImagePath)!
+        
+        let img1 = CIImage(contentsOf: firstImageURL)
+        let img2 = CIImage(contentsOf: secondImageURL)
+        
+        let filter = CIFilter(name: "CIAdditionCompositing")!
+        filter.setDefaults()
+        
+        filter.setValue(img1, forKey: "inputImage")
+        filter.setValue(img2, forKey: "inputBackgroundImage")
+        
+        let resultImage = filter.outputImage
+        
+        let rep = NSCIImageRep(ciImage: resultImage!)
+        let finalResult = NSImage(size: rep.size)
+        finalResult.addRepresentation(rep)
+        
+        finalResult //
+    }
+}
--- ImageMerge/main.swift
+++ ImageMerge/main.swift
@@ -7,6 +7,26 @@
 //
 
 import Foundation
+import AppKit
+import CoreImage
 
-print("Hello, World!")
+let images = Images()
+images.generate()
 
+//if let p = path {
+//    let url = URL(string: p)!
+//    print(url)
+//} else {
+//    print("Could not find path")
+//}
+
+//struct Images {
+//    static func mergeImages(imageView: NSImageView) -> NSImage {
+//        UIGraphicsBeginImageContextWithOptions(imageView.frame.size, false, 0.0)
+//        imageView.superview!.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+//        let image = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        
+//        return image 
+//    }
+//}
